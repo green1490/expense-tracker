88bf903e5679ab77ec1c4be90b44fe470ecf44ae
green1490 <zambokrisztian1490@gmail.com>
2022-09-18 20:00:27 +0200

    Extend view model with new variables.

--- app/src/main/java/com/example/expensetracker/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/expensetracker/ui/home/HomeViewModel.kt
@@ -3,11 +3,64 @@ package com.example.expensetracker.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.util.*
 
 class HomeViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
+     private  val _title = MutableLiveData<String>().apply {
         value = "This is home Fragment"
     }
-    val text: LiveData<String> = _text
+
+    private  val _sum = MutableLiveData<Int>().apply {
+        value = 0
+    }
+
+    private val _date = MutableLiveData<Date>().apply {
+        value = Date()
+    }
+
+    private val _labels = MutableLiveData<MutableList<String>>().apply {
+        value = mutableListOf()
+    }
+
+    private val _description = MutableLiveData<String>().apply {
+        value = String()
+    }
+
+    private val _balance = MutableLiveData<UInt>().apply {
+        value = UInt.MIN_VALUE
+    }
+
+    fun setTitle(text:String) {
+        _title.value = text
+    }
+
+    fun setSum(value:Int) {
+        _sum.value = value
+    }
+
+    fun setDate(date:Date) {
+        _date.value = date
+    }
+
+    fun setLabels(labels:MutableList<String>) {
+        _labels.value = labels
+    }
+
+    fun setDescription(text:String) {
+        _description.value = text
+    }
+
+    fun setBalance(balance:UInt) {
+        _balance.value = balance
+    }
+
+
+    // bank identifier variable
+    val title:          LiveData<String> = _title
+    val sum:            LiveData<Int> = _sum
+    val date:           LiveData<Date> = _date
+    val labels:         LiveData<MutableList<String>> = _labels
+    val description:    LiveData<String> = _description
+    val balance:        LiveData<UInt> = _balance
 }
