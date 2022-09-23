52cb4ef9a30edfab204eb09e7399ba4d5791c848
green1490 <zambokrisztian1490@gmail.com>
2022-09-23 18:36:21 +0200

    test

--- app/src/main/java/com/example/expensetracker/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/expensetracker/ui/home/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -12,6 +13,8 @@ import com.example.expensetracker.databinding.FragmentHomeBinding
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
+    private val _fileName:String = "history"
+    private lateinit var homeViewModel:HomeViewModel
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,21 +25,28 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
+        homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        homeViewModel.balancePercentage.observe(viewLifecycleOwner) {
+            textView.text = it.toString()
         }
+
+        val progressBar: ProgressBar = binding.progressBar
+        progressBar.progress = homeViewModel.balancePercentage.value!!
+
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+//        context.openFileOutput(_fileName, Context.MODE_PRIVATER).use {
+//          it.write()
+//        }
         _binding = null
     }
 }
--- app/src/main/java/com/example/expensetracker/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/expensetracker/ui/home/HomeViewModel.kt
@@ -3,20 +3,21 @@ package com.example.expensetracker.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.time.LocalDate
 import java.util.*
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel: ViewModel() {
 
-     private  val _title = MutableLiveData<String>().apply {
+    private val _title = MutableLiveData<String>().apply {
         value = "This is home Fragment"
     }
 
-    private  val _sum = MutableLiveData<Int>().apply {
+    private val _sum = MutableLiveData<Int>().apply {
         value = 0
     }
 
-    private val _date = MutableLiveData<Date>().apply {
-        value = Date()
+    private val _date = MutableLiveData<LocalDate>().apply {
+        value = LocalDate.now()
     }
 
     private val _labels = MutableLiveData<MutableList<String>>().apply {
@@ -31,6 +32,14 @@ class HomeViewModel : ViewModel() {
         value = UInt.MIN_VALUE
     }
 
+    private val _maxPayment = MutableLiveData<UInt>().apply {
+        value = UInt.MIN_VALUE
+    }
+
+    private val _balancePercentage = MutableLiveData<Int>().apply {
+        value = 0
+    }
+
     fun setTitle(text:String) {
         _title.value = text
     }
@@ -39,7 +48,7 @@ class HomeViewModel : ViewModel() {
         _sum.value = value
     }
 
-    fun setDate(date:Date) {
+    fun setDate(date:LocalDate) {
         _date.value = date
     }
 
@@ -55,12 +64,25 @@ class HomeViewModel : ViewModel() {
         _balance.value = balance
     }
 
+    init {
+        _title.value = "Test buying"
+        _sum.value = 4000
+        _date.value = LocalDate.now()
+        _labels.value?.addAll(mutableListOf("food","electronic"))
+        _description.value = "Test for desc"
+        _maxPayment.value = 6000u
+        _balance.value = 5000u
+        _balancePercentage.value = (_balance.value!!.toInt() / _maxPayment.value!!.toInt()) * 100
+    }
+
 
     // bank identifier variable
     val title:          LiveData<String> = _title
     val sum:            LiveData<Int> = _sum
-    val date:           LiveData<Date> = _date
+    val date:           LiveData<LocalDate> = _date
     val labels:         LiveData<MutableList<String>> = _labels
     val description:    LiveData<String> = _description
     val balance:        LiveData<UInt> = _balance
+    val maxPayment:     LiveData<UInt> = _maxPayment
+    val balancePercentage:     LiveData<Int> = _balancePercentage
 }
