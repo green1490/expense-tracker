9a7eadfe0932380ddd98401e611a93d58f2a9c3f
green1490 <zambokrisztian1490@gmail.com>
2022-09-27 08:33:54 +0200

    change notification to setting

--- app/src/main/java/com/example/expensetracker/ui/settings/SettingFragment.kt
+++ app/src/main/java/com/example/expensetracker/ui/settings/SettingFragment.kt
@@ -1,4 +1,4 @@
-package com.example.expensetracker.ui.notifications
+package com.example.expensetracker.ui.settings
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.expensetracker.databinding.FragmentNotificationsBinding
+import com.example.expensetracker.databinding.FragmentSettingsBinding
 
-class NotificationsFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+class SettingFragment : Fragment() {
+
+    private var _binding: FragmentSettingsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +23,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val settingViewModel =
+            ViewModelProvider(this).get(SettingViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        settingViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
