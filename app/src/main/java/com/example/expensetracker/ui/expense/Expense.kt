26691af80b0d5534d833177b4da81bce5e55952e
green1490 <zambokrisztian1490@gmail.com>
2022-09-27 10:26:00 +0200

    Add components

--- app/src/main/java/com/example/expensetracker/ui/expense/Expense.kt
+++ app/src/main/java/com/example/expensetracker/ui/expense/Expense.kt
@@ -2,11 +2,21 @@ package com.example.expensetracker.ui.expense
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.expensetracker.R
 
 class Expense : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_expense)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val button = findViewById<Button>(R.id.expense_save)
+        button.setOnClickListener {
+            //TO DO: extract data and save it
+            finish()
+        }
     }
 }
