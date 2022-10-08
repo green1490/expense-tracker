101289a880ae341c52fd07ae4d0a93090cb97e5b
green1490 <zambokrisztian1490@gmail.com>
2022-10-08 10:40:57 +0200

    implement fingerprint sensor for accessing the application
    Signed-off-by: green1490 <zambokrisztian1490@gmail.com>

--- app/src/main/java/com/example/expensetracker/MainActivity.kt
+++ app/src/main/java/com/example/expensetracker/MainActivity.kt
@@ -1,20 +1,62 @@
 package com.example.expensetracker
 
+import android.app.KeyguardManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.hardware.biometrics.BiometricPrompt
 import android.os.Bundle
+import android.os.CancellationSignal
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.expensetracker.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var cancellationSignal:CancellationSignal? = null
+    private val authentetionCallBack:BiometricPrompt.AuthenticationCallback
+    get() =
+        object: BiometricPrompt.AuthenticationCallback() {
+            override fun onAuthenticationError(errorCode: Int, errString: CharSequence?) {
+                super.onAuthenticationError(errorCode, errString)
+                notifyUser("Authentication error: $errString")
+                this@MainActivity.finish()
+                exitProcess(0)
+            }
+
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult?) {
+                super.onAuthenticationSucceeded(result)
+                notifyUser("Authentication success!")
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (!checkBiometricSupport()) {
+            this@MainActivity.finish()
+            exitProcess(0)
+        }
+
+        val biometricPrompt = BiometricPrompt.Builder(this)
+            .setTitle("Title of prompt")
+            .setSubtitle("Authentication is required")
+            .setDescription("This app uses fingerprint to keep data secure!")
+            .setNegativeButton("Cancel",this.mainExecutor) { dialog, which ->
+                notifyUser("Authentication cancelled")
+                this@MainActivity.finish()
+                exitProcess(0)
+            }.build()
+
+        biometricPrompt.authenticate(getCancellationSignal(),mainExecutor,authentetionCallBack)
+
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -31,5 +73,39 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    private fun notifyUser(message:String) {
+        Toast.makeText(this,message,Toast.LENGTH_LONG).show()
+    }
+
+    private fun getCancellationSignal():CancellationSignal {
+        cancellationSignal = CancellationSignal()
+        cancellationSignal?.setOnCancelListener {
+            notifyUser("Authentication was cancelled by the user!")
+            this@MainActivity.finish()
+            exitProcess(0)
+        }
+        return cancellationSignal as CancellationSignal
+    }
+
+    private fun checkBiometricSupport():Boolean{
+        val keyguardManager = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+
+        if(!keyguardManager.isKeyguardSecure) {
+            notifyUser("Fingerprint authentication has not been enabled!")
+            return false
+        }
+
+        if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.USE_BIOMETRIC) != PackageManager.PERMISSION_GRANTED){
+            notifyUser("Fingerprint authentication is not enabled!")
+            return false
+        }
+        return if(packageManager.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT)) {
+            return true
+        }
+        else {
+            true
+        }
     }
 }
